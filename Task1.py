2d98fa6d5ccf9c77b76792716c19ee17ae1f5adb
swarnambika <119099951+Swarnasy@users.noreply.github.com>
2023-03-01 09:51:50 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.header("Innomatics Internship-23")
 st.title(":red[Task-1]")
-st.subheader("First Streamlit App:cake:")
+st.subheader("My First Streamlit App")
+with st.sidebar:
+    st.title["About"]
 
+clicked=st.button('Click me')
+st.balloons()
 
